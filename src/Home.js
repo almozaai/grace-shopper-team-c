830023bf79c5a78f29d44f6cc633871fd45350c3
almozaai <info@salehsaeed.com>
2019-10-30 08:16:10 +0300

    fix bugs in Search and add thunk and action addToCart

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,28 @@
-import { render } from 'react-dom';
-import React, { Component } from 'react';
-import { HashRouter, Route, Link, Switch, Redirect, NavLink } from 'react-router-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider, connect } from 'react-redux';
+import { render } from "react-dom";
+import React, { Component } from "react";
+import {
+  HashRouter,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  NavLink
+} from "react-router-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
 import thunkMiddleware from "redux-thunk";
-import axios from "axios"
+import axios from "axios";
+import Search from "./Search";
 
 const _Home = () => {
   return (
-    <h1>Home Page</h1>
-  )
-}
+    <div>
+      <h1>Home Page</h1>
+      <Search />
+    </div>
+  );
+};
 
 const Home = connect()(_Home);
 
-export default Home
+export default Home;
---
+++ src/Search.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { connect } from "react-redux";
+import Products from "./Products";
+
+class _Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchItems: [],
+      input: ""
+    };
+  }
+  startSearch(e) {
+    const searchItems = this.props.products.filter(product =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    this.setState({ searchItems, input: e.target.value });
+  }
+
+  render() {
+    const { searchItems, input } = this.state;
+
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            onKeyUp={e => this.startSearch(e)}
+          />
+        </form>
+        <div className="ordering">
+          {searchItems.length === 0 && !input ? (
+            <Products />
+          ) : (
+            searchItems.map(product => (
+              <div key={product.id} className="border">
+                {product.name}
+                <br />${product.price}
+                <br />
+                <button onClick={() => this.props.toCreate(product)}>
+                  Add to Cart
+                </button>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+const Search = connect(({ products }) => {
+  return {
+    products
+  };
+})(_Search);
+
+export default Search;
